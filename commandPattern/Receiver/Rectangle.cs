75bb989016984b5735ad6ce96a6536a088999bc4
jeevan <jeevan90wijerathna@gmail.com>
2019-05-20 23:50:01 +0800

    unit tests

--- commandPattern/Receiver/Rectangle.cs
+++ commandPattern/Receiver/Rectangle.cs
@@ -2,14 +2,18 @@ namespace designPattern.Receiver {
     public class Rectangle {
         private int _height;
         private int _width;
+
+        public int Height { get => _height; set => _height = value; }
+        public int Width { get => _width; set => _width = value; }
+
         public void SetHeight (int height) {
-            this._height = height;
-            System.Console.WriteLine($"Height is:{this._height}");
+            this.Height = height;
+            System.Console.WriteLine($"Height is:{this.Height}");
         }
 
         public void SetWidth (int width) {
-            this._width = width;
-            System.Console.WriteLine($"Width is:{this._height}");
+            this.Width = width;
+            System.Console.WriteLine($"Width is:{this.Height}");
         }
     }
 }

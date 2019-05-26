c48e9a6f9f9ba9c1bc3cde4c0706b789ad2f63da
Jeevan Wijerathna <jeevan90wijerathna@gmail.com>
2019-05-26 19:55:14 +0800

    command pattern undo

--- commandPattern/Commands/SetHeightCommand.cs
+++ commandPattern/Commands/SetHeightCommand.cs
@@ -1,14 +1,26 @@
+using System;
 using designPattern.Abstraction;
 using designPattern.Receiver;
 
 namespace designPattern.Commands {
     public class SetHeightCommand : ICommand {
         private readonly Rectangle _rectangle;
-        private readonly int _height;
+        private int _height;
+        private int _oldValue;
         public SetHeightCommand (Rectangle rectangle, int height) {
             this._height = height;
             this._rectangle = rectangle;
+            
         }
-        public void Execute() => _rectangle.SetHeight(_height);
+        public void Execute()
+        {
+            this._oldValue = _rectangle.Height;
+            _rectangle.SetHeight(_height);
+        }
+
+        public void Undo()
+        {
+           this._height = _oldValue;
+        }
     }
 }
